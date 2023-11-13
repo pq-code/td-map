4719a702be7f9724f05aa180699cb1135af584e5
p q <784312297@qq.com>
2023-11-13 17:16:06 +0800

    feat: 新增mark

--- src/components/map/td-ol/hooks/useTMapLoader.ts
+++ src/components/map/td-ol/hooks/useTMapLoader.ts
@@ -1,26 +1,54 @@
+import { ref } from "vue";
+import { buildUUID } from "@/utils/index";
 
 import 'ol/ol.css'
-import { Map, View} from 'ol'
+import { Map, View, Feature } from 'ol'
 import * as olSource from 'ol/source';
-import TileLayer from 'ol/layer/Tile';
+// import TileLayer from 'ol/layer/Tile';
 
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { XYZ, Vector as VectorSource } from "ol/source";
+import * as olProj from "ol/proj";
+import { Point } from "ol/geom";
+import { Style, Fill, Stroke, Circle, Icon } from "ol/style";
 
-const typeMapping:object = {
-    img: 'http://t{0-7}.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=',
-    cia: 'http://t{0-7}.tianditu.com/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=',
-    vec: 'http://t{0-7}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=',
-    cva: 'http://t{0-7}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk='
-} 
+const mapId = ref(buildUUID());
+interface options {
+  key: string; // 申请好的Web端开发者Key，首次调用 load 时必填
+  target: string; // 容器id
+  version?: string; // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+  plugins?: string[]; //插件列表
+  center?: Array<number> // 中心点
+  zoom?: number
+  projection?: string
+  // 是否加载 AMapUI，缺省不加载
+  AMapUI?: {
+    version?: string; // AMapUI 缺省 1.1
+    plugins?: string[]; // 需要加载的 AMapUI ui插件
+  };
+  // 是否加载 Loca， 缺省不加载
+  Loca?: {
+    version?: string; // Loca 版本，缺省 1.3.2
+  };
+}
 
-export function olLoad() {
-  let Omap;
+const typeMapping: object = {
+  img: 'http://t{0-7}.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=',
+  cia: 'http://t{0-7}.tianditu.com/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=',
+  vec: 'http://t{0-7}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=',
+  cva: 'http://t{0-7}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk='
+}
 
-  const load = (options:options)=>{
-    const { key ,plugins} = options
+let Omap: any
+
+export default function useTMapLoader() {
+
+  const tdOLoad = (options: options) => {
+    const { key, plugins = ["vec", "cva"], target, center = [120.159, 30.26], zoom = 10, projection = "EPSG:4326" } = options
     // 匹配地图类型
-    let layers = []
+    let layers: any = []
 
-    plugins?.forEach(e=>{
+    plugins?.forEach((e: string) => {
       layers.push(
         new TileLayer({
           source: new olSource.XYZ({
@@ -30,41 +58,66 @@ export function olLoad() {
       )
     })
 
-    return new Promise((res,rej)=>{
-      // 创建一个地图
-      Omap = new Map({
+    return new Promise((res, rej) => {
+      res({
         // 指向目标容器
-        // target,
+        target,
         // 加载图层
-        // layers: [TiandiMap_img,TiandiMap_cia],
-        layers: [],
+        layers,
         // 设置视图
-          view: new View({
-              // center: ol.proj.transform([116.2, 39.56], 'EPSG:4326', 'EPSG:3857'),
-            center: [120.159, 30.26],// 中心点坐标
-            zoom: 10, // 缩放等级
-            projection: "EPSG:4326" // 坐标系
-          })
+        view: new View({
+          // center: ol.proj.transform([116.2, 39.56], 'EPSG:4326', 'EPSG:3857'),
+          center,// 中心点坐标
+          zoom, // 缩放等级
+          projection // 坐标系
         })
       })
-    }
+    })
+  }
+
+  // 获取map对象
+  const setMap = (map: any) => {
+    Omap = map
+    //创建矢量层用来装后续添加的图形
+
+  }
+
+  // 添加点
+  const setMarker = (drop: object) => {
+    const styles = [
+      new Style({
+        image: drop.content
+      }),
+    ];
+    // 创建矢量对象
+    let feature = new Feature({
+      geometry: drop.position,
+    });
+    // 创建矢量源
+    let source = new VectorSource({ wrapX: false });
+    // 把要素集合添加到源 addFeatures
+    source.addFeature(feature);
+    // 创建矢量层
+    let vector = new VectorLayer({
+      source: source,
+      style: styles,
+      id: `marker${mapId}`,
+    });
+    // 把源添加到地图
+    Omap.addLayer(vector);
+    console.log(Omap)
+  }
+
+  const setPolygon = () => {
+
+  }
+
   return {
-    load
+    tdOLoad,
+    setMap,
+    setMarker,
+    setPolygon
   }
 }
 
-interface options  {
-  key: string; // 申请好的Web端开发者Key，首次调用 load 时必填
-  version: string; // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-  plugins?: string[]; //插件列表
-  // 是否加载 AMapUI，缺省不加载
-  AMapUI?: {
-      version?: string; // AMapUI 缺省 1.1
-      plugins?: string[]; // 需要加载的 AMapUI ui插件
-  };
-  // 是否加载 Loca， 缺省不加载
-  Loca?: {
-      version?: string; // Loca 版本，缺省 1.3.2
-  };
-}
 
--- src/components/map/td-ol/td-map.vue
+++ src/components/map/td-ol/td-map.vue
@@ -1,87 +1,82 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { buildUUID } from '../../../utils/index'
-import { olLoad } from './hooks/useTMapLoader'
+import { ref, onMounted } from "vue";
+import { buildUUID } from "../../../utils/index";
+import useTMapLoader from "./hooks/useTMapLoader";
+import { Map, View, Feature } from "ol";
 
-import 'ol/ol.css'
-import { Map, View } from 'ol'
-import * as olSource from 'ol/source'
-import TileLayer from 'ol/layer/Tile'
-// import WMTS from 'ol/source/WMTS';
-// import WMTSTileGrid from 'ol/tilegrid/WMTS';
-// import { get as getProjection } from 'ol/proj';
-// import { getWidth, getTopLeft } from 'ol/extent';
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { XYZ, Vector as VectorSource } from "ol/source";
+import * as olProj from "ol/proj";
+import { Point } from "ol/geom";
+import { Style, Fill, Stroke, Circle, Icon } from "ol/style";
+let map = ref();
+const { tdOLoad, setMarker, setMap, setPolygon } = useTMapLoader();
+const mapId = ref(buildUUID());
 
-const MapKey = '6177ed85f51008870c8e12d50c4e35a2'
-const mapId = ref(buildUUID())
-
-// 初始化一个空地图
-const map = ref()
-
-var TiandiMap_img = new TileLayer({
-  name: '天地图影像图层',
-  source: new olSource.XYZ({
-    url: 'http://t0.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=' + MapKey + '',
-    wrapX: false
-  })
-})
-var TiandiMap_cia = new TileLayer({
-  name: '天地图影像注记图层',
-  source: new olSource.XYZ({
-    url: 'http://t0.tianditu.com/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=' + MapKey + '',
-    wrapX: false
-  })
-})
-
-var vec_w = new TileLayer({
-  name: '矢量底图',
-  source: new olSource.XYZ({
-    url: 'http://t{0-7}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=' + MapKey + ''
-  })
-})
-
-var cva_w = new TileLayer({
-  name: '矢量图文字标注',
-  source: new olSource.XYZ({
-    url: 'http://t{0-7}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=' + MapKey + ''
-  })
-})
-
-// 初始化地图函数
-function initMap() {
-  // 创建一个地图
-  map.value = new Map({
-    // 指向目标容器
-    target: `${mapId.value}_content`,
-    // 加载图层
-    // layers: [TiandiMap_img,TiandiMap_cia],
-    layers: [vec_w, cva_w],
-    // 设置视图
-    view: new View({
-      // center: ol.proj.transform([116.2, 39.56], 'EPSG:4326', 'EPSG:3857'),
-      center: [120.159, 30.26], // 中心点坐标
-      zoom: 10, // 缩放等级
-      projection: 'EPSG:4326' // 坐标系
-    })
-  })
-}
-// 组件挂载时初始化地图
 onMounted(() => {
-  //   initMap();
-  const { load } = olLoad
-  load({
-    key: '6177ed85f51008870c8e12d50c4e35a2',
+  tdOLoad({
+    key: "6177ed85f51008870c8e12d50c4e35a2",
     target: `${mapId.value}_content`,
-    plugins: ['vec', 'cva']
-  }).then((Omap) => {})
-})
+    plugins: ["vec", "cva"],
+    center: [120.159, 30.26],
+    zoom: 15,
+  }).then((Omap: any) => {
+    map.value = new Map(Omap);
+    setMap(map.value);
+    // 添加多边形
+    batchSetMarker([
+      [120.159, 30.26],
+      [120.169, 30.26],
+      [120.169, 30.27],
+      [120.163, 30.27],
+    ]);
+  });
+});
+
+// 添加mark
+const batchSetMarker = (drop: Array<Array<number>> | Array<object>) => {
+  for (let item of drop) {
+    setMarker({
+      position: new Point(item),
+      content: new Icon({
+        src: "https://a.amap.com/lbs/static/img/doc/doc_1678970777168_d2b5c.png",
+        scale: 0.8,
+      }),
+    });
+  }
+};
+// 添加多边形
+const pathArr = [
+  [
+    [
+      [121.7789, 31.3102],
+      [121.7279, 31.3548],
+      [121.5723, 31.4361],
+      [121.5093, 31.4898],
+      [121.5624, 31.4864],
+      [121.5856, 31.4547],
+      [121.7694, 31.3907],
+      [121.796, 31.3456],
+      [121.7789, 31.3102],
+    ],
+  ],
+];
+setPolygon();
+// const polygon = new AMap.Polygon({
+//   path: pathArr,
+//   fillColor: "#ccebc5", // 多边形填充颜色
+//   strokeOpacity: 1, // 线条透明度
+//   fillOpacity: 0.5, //填充透明度
+//   strokeColor: "#2b8cbe", // 线条颜色
+//   strokeWeight: 1, //线条宽度
+//   strokeStyle: "dashed", // 线样式
+//   strokeDasharray: [5, 5], //轮廓的虚线和间隙的样式
+// });
 </script>
 <template>
-  <!-- <div class="openlayers">
-        openlayers
-    </div> -->
   <div :id="`${mapId}_content`" class="content"></div>
 </template>
+
 <style lang="less" scoped>
 .openlayers {
   position: absolute;
@@ -94,6 +89,7 @@ onMounted(() => {
   color: #ffffff;
   background-color: #000000;
 }
+
 .content {
   height: 100vh;
   width: 100vw;
